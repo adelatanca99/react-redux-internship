d6ab1b933c86c94ef8ef9d30e60fa444b92fc900
Adela Tanca <adela.tanca@lateral-inc.com>
2022-08-04 11:43:39 +0300

    wip redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,34 @@
-// Action creator
-export const selectSong = (song) => {
-    // Return an action
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+//FOR SONGS
 
-    };
+// // Action creator
+// export const selectSong = (song) => {
+//     // Return an action
+//     return {
+//         type: 'SONG_SELECTED',
+//         payload: song
+
+//     };
+// };
+
+// FOR FETCH PART
+
+
+// export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+//     await dispatch(fetchPosts());
+
+//     const userIds = _.uniq(_.map(getState().posts, 'userId'))
+// }
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -8,25 +8,25 @@ import SongDetail from './SongDetail';
 import PostList from './PostList';
 
 
-const App = () => {
-    return (
-        <div className='ui container grid'>
-            <div className='ui row'>
-                <div className='column eight wide'>
-                    <SongList />
-                </div>
-                <div className='column eight wide'>
-                    <SongDetail />
-                </div>
-            </div>
-            <Counter />
-            <Component1 />
-        </div>
-    )
-};
-
 // const App = () => {
-//     return <div className='ui cotainer'><PostList /></div>
-// }
+//     return (
+//         <div className='ui container grid'>
+//             <div className='ui row'>
+//                 <div className='column eight wide'>
+//                     <SongList />
+//                 </div>
+//                 <div className='column eight wide'>
+//                     <SongDetail />
+//                 </div>
+//             </div>
+//             <Counter />
+//             <Component1 />
+//         </div>
+//     )
+// };
+
+const App = () => {
+    return <div className='ui cotainer'><PostList /></div>
+}
 
 export default App;
